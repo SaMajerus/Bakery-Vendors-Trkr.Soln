81373a355677642087b426e5bb1761b17917cbbd
Samuel Majerus <ladolego@gmail.com>
2022-09-30 15:42:58 -0700

    Adds block of commented-code for reference from 'BreadTests.cs'.

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -5,19 +5,20 @@ using System;
 
 namespace Bakery.Tests
 {
-/*  [TestClass]
-  public class ItemTests : IDisposable
-  {
 
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+  /*
     public void Dispose(){
-      Item.ClearAll();
+      Order.ClearAll();
     }
 
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Item newItem = new Item("test");  
-      Assert.AreEqual(typeof(Item), newItem.GetType());
+      Order newOrder = new Order("test");  
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     
     [TestMethod]
@@ -25,10 +26,10 @@ namespace Bakery.Tests
     {
       //Arrange
       string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      Order newOrder = new Order(description);
 
       //Act
-      string result = newItem.Description;
+      string result = newOrder.Description;
 
       //Assert
       Assert.AreEqual(description, result);
@@ -39,75 +40,114 @@ namespace Bakery.Tests
     {
       //Arrange
       string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      Order newOrder = new Order(description);
 
       //Act
       string updatedDescription = "Do the dishes";
-      newItem.Description = updatedDescription;
-      string result = newItem.Description;
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
 
       //Assert
       Assert.AreEqual(updatedDescription, result);
     }
     
     [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
+    public void GetAll_ReturnsEmptyList_OrderList()
     {
       // Arrange
-      List<Item> newList = new List<Item> { };
+      List<Order> newList = new List<Order> { };
 
       // Act
-      List<Item> result = Item.GetAll();
+      List<Order> result = Order.GetAll();
 
       // Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
+    public void GetAll_ReturnsOrders_OrderList()
     {
       //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
 
       //Act
-      List<Item> result = Item.GetAll();
+      List<Order> result = Order.GetAll();
 
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
-    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
     {
       //Arrange
       string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      Order newOrder = new Order(description);
 
       //Act
-      int result = newItem.Id;
+      int result = newOrder.Id;
 
       //Assert
       Assert.AreEqual(1, result);
     }
 
     [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
+    public void Find_ReturnsCorrectOrder_Order()
     {
       //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
 
       //Act
-      Item result = Item.Find(2);
+      Order result = Order.Find(2);
 
       //Assert
-      Assert.AreEqual(newItem2, result);
-    }
-  }*/
+      Assert.AreEqual(newOrder2, result);
+    }*/
+  }
 }
+
+
+
+
+
+
+    /*
+    [TestMethod]
+    public void DealApplies_CheckCompatabilityWithBreadDeal_False()
+    {
+      Bread test = new Bread(2);
+      Assert.AreEqual(false, test.DealApplies());
+    }
+
+    [TestMethod]
+    public void DealApplies_UseBranchingToCheckBreadDealCompatability_True()
+    {
+      Bread test1 = new Bread(1);
+      Bread test2 = new Bread(2);
+      Bread test3 = new Bread(3);
+      Assert.AreEqual(false, test1.DealApplies());
+      Assert.AreEqual(false, test2.DealApplies());
+      Assert.AreEqual(true, test3.DealApplies());
+    }
+
+    [TestMethod]
+    public void DetermineTotalPrice_UseBranchingToMakeCalculationsBasedOnQuantity_Int()
+    { 
+      Bread test1 = new Bread(1); 
+      Bread test2 = new Bread(2); 
+      Bread test3 = new Bread(3); 
+      Bread test4 = new Bread(4);
+      Bread test5 = new Bread(8); 
+      Assert.AreEqual(5, test1.DetermineTotalPrice()); 
+      Assert.AreEqual(10, test2.DetermineTotalPrice()); 
+      Assert.AreEqual(10, test3.DetermineTotalPrice()); 
+      Assert.AreEqual(15, test4.DetermineTotalPrice()); 
+      Assert.AreEqual(30, test5.DetermineTotalPrice()); 
+    } */ 
