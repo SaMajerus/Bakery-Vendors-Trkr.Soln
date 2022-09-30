cf96c00690a63622cfb17b3289f448aab2255346
Samuel Majerus <ladolego@gmail.com>
2022-09-30 15:39:07 -0700

    Adds iteration of 'Vendor.cs' that is block-commented and ready for proper TDD workflow.

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -1,8 +1,36 @@
-using System;
+using System.Collections.Generic;
 
-/*     Business Logic ('Bread')     */ 
 namespace Bakery.Models
 {
+  public class Vendor
+  {
+    //private static List<Vendor> _vendors = new List<Vendor> {}; 
+    public string VendorName { get; set; }
+    public int Id { get; }
+    //public List<Order> Orders { get; set; }
+    public List<Order> Orders = new List<Order> {}; 
+/*
+    public Vendor(string name) {
+      VendorName = name;
+      _vendors.Add(this);
+      Id = _vendors.Count;
+      //Orders = new List<Order> {};
+    }
 
-  
+    public static void ClearAll() {
+      _vendors.Clear();
+    }
+
+    public static List<Vendor> GetAll() {
+      return _vendors;
+    }
+
+    public static Vendor Find(int searchId) {
+      return _vendors[searchId-1];
+    }
+
+    public void AddVendor(Vendor ven) {
+      Vendors.Add(ven);
+    }*/
+  }
 }
