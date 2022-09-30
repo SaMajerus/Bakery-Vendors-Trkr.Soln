86bab21a081f7f986217d45908b1410e91daffd0
Samuel Majerus <ladolego@gmail.com>
2022-09-30 16:52:20 -0700

    OrderTests -- Test 1, good fail.

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -2,36 +2,29 @@ using System.Collections.Generic;
 
 namespace Bakery.Models{
   public class Order{
-    
+
     private static List<Order> _orders = new List<Order> {}; 
     public string OrderName { get; set; }
     public string Description { get; set; }
-    public int totalPrice { get; set; } 
+    public int TotalPrice { get; set; } 
     public int DateInput { get; set; } 
-    public int Id { get; } 
-    // public int QtyBread { get; set; } 
-    // public int QtyPastry { get; set; } 
-    // public int BreadPrice { get; set; }
-    // public int PastryPrice { get; set; } 
-    public int Date { get; set; } 
+    public int Id { get; }  
 
-/*
-    public Order(string name, string description, int totalPrice, int d8) { /*
+    public Order(string name, string description, int totalPrice, int d8) { 
       OrderName = name; 
-      QtyBread = qtyBread; 
-      QtyPastry = qtyPastry; 
-      BreadPrice = 5;  //Price IFF (if and only if) Quantity=1. 
-      PastryPrice = 2;  //Price IFF (if and only if) Quantity=1. 
+      Description = description; 
+      TotalPrice = totalPrice; 
+      DateInput = d8; 
       _orders.Add(this);
       Id = _orders.Count; 
     } 
-*/
 
 
-/*
+
+
     public static List<Order> GetAll()  //'Getter' method
     {
-      return _orders;
+//      return _orders;
     }
 
     public static void ClearAll()
@@ -43,7 +36,6 @@ namespace Bakery.Models{
     {
       return _orders[searchId-1];
     }
-*/
 
   }
 }
