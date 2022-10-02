5126c0ed86a320aecbb481b6d6cb602c4a0d83b9
Samuel Majerus <ladolego@gmail.com>
2022-10-01 21:13:39 -0700

    Order Test 7 -- pass.  ('OrderTests.cs' 100-percent passing.)   [WIP:  'VendorTests.cs' file tests]

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -94,22 +94,22 @@ namespace Bakery.Tests
       //Assert
       Assert.AreEqual(1, result);
     }
-/*
+
     [TestMethod]
     public void Find_ReturnsCorrectOrder_Order()
     {
       //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Order newOrder1 = new Order(description01);
-      Order newOrder2 = new Order(description02);
+      string description01 = "100 Bread Loaves, 50 Pastries";
+      string description02 = "5 Bread Loaves, 5 Pastries";
+      Order newOrder1 = new Order("Order 1", description01, 419, 10012022);
+      Order newOrder2 = new Order("Order 2", description02, 29, 10012022);
 
       //Act
       Order result = Order.Find(2);
 
       //Assert
       Assert.AreEqual(newOrder2, result);
-    }*/
+    }
   }
 }
 
