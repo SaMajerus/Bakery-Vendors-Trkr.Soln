f61e235d2b95bb3b807cf11f4c212207f1cadafa
Samuel Majerus <ladolego@gmail.com>
2022-10-02 12:37:12 -0700

    Vendor Test 5 -- pass.

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -81,13 +81,13 @@ namespace Bakery.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
-/*
+
     [TestMethod]
     public void GetId_VendorsInstantiateWithAnIdAndGetterReturns_Int()
     {
       //Arrange
-      string description = "Walk the dog.";
-      Vendor newVendor = new Vendor(description);
+      string description = "Coffee shop with a bakery mini-stand";
+      Vendor newVendor = new Vendor("Delectables and Coffee!", description); 
 
       //Act
       int result = newVendor.Id;
@@ -95,9 +95,25 @@ namespace Bakery.Tests
       //Assert
       Assert.AreEqual(1, result);
     }
-
+/*
     [TestMethod]
     public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      //Arrange
+      string description01 = "Open-air coffee shop/cafe";
+      string description02 = "Street shop that sells loaves of bread and pastries to breakfast makers";
+      Vendor newVendor1 = new Vendor("Annie's Coffee Bar & Cafe", description01);
+      Vendor newVendor2 = new Vendor("Simple Italian Breakfasts", description02);
+
+      //Act
+      Vendor result = Vendor.Find(2);
+
+      //Assert
+      Assert.AreEqual(newVendor2, result);
+    }
+
+    [TestMethod]
+    public void Show_ReturnsCorrectOrderListFromVendor_OrderList()
     {
       //Arrange
       string description01 = "Walk the dog";
