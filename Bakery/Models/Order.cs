1ee7814add0a2654dd15164327a213c375801aa4
Samuel Majerus <ladolego@gmail.com>
2022-10-02 13:24:52 -0700

    Updates/Adds constructor fields for 'Order' object-- 1st is 'date submitted', 2nd is 'Deliver-By' date.   [WIP: Refactoring 'new object' definitions/calls in Test files.]

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -7,20 +7,19 @@ namespace Bakery.Models{
     public string OrderName { get; set; }
     public string Description { get; set; }
     public int TotalPrice { get; set; } 
-    public int DateInput { get; set; } 
+    public int DateSubmitted { get; set; }  //Format:  MMDDYYYY
+    public int DeliverBy { get; set; }  //Format: same as above
     public int Id { get; }  
 
-    public Order(string name, string description, int totalPrice, int d8) { 
-      OrderName = name; 
-      Description = description; 
-      TotalPrice = totalPrice; 
-      DateInput = d8; 
-      _orders.Add(this);
+    public Order(string name, string description, int totalPrice, int d8Added, int deadline) { 
+      OrderName = name;  //i.e. '[Purpose of order]' 
+      Description = description;  //Requested quantity of each Item 
+      TotalPrice = totalPrice;  //Grand Total Price (based on 'Receipt' printout   from Console App program) 
+      DateSubmitted = d8Added;  //Date the order was placed.
+      DeliverBy = deadline;  //Date by which the order needs to be delivered. (Always at 6am, let's say) 
+      _orders.Add(this); 
       Id = _orders.Count; 
     } 
-
-
-
 
     public static List<Order> GetAll()  //'Getter' method
     {
